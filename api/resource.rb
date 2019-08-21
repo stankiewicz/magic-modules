6ea22dc0292a29f2a8d15fe74941182a251e3684
Alex Stephen <alexstephen@google.com>
2019-08-21 14:26:40 -0700

    removing encoder? and decoder?

--- api/resource.rb
+++ api/resource.rb
@@ -256,14 +256,6 @@ module Api
       !@kind.nil?
     end
 
-    def encoder?
-      !@transport&.encoder.nil?
-    end
-
-    def decoder?
-      !@transport&.decoders.empty?
-    end
-
     # ====================
     # Version-related methods
     # ====================
